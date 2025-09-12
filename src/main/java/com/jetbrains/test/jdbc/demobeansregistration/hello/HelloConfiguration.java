013080112016b8a77da286aa75f86093ab802c5f
Andrey Belyaev <andrey.belyaev@jetbrains.com>
2025-09-12 17:26:03 +0200

    Added case for further testing

--- src/main/java/com/jetbrains/test/jdbc/demobeansregistration/hello/HelloConfiguration.java
+++ src/main/java/com/jetbrains/test/jdbc/demobeansregistration/hello/HelloConfiguration.java
@@ -1,10 +1,16 @@
 package com.jetbrains.test.jdbc.demobeansregistration.hello;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
 @Import(HelloServiceRegistrar.class)
 class HelloConfiguration {
+
+    @Bean(name = "myNewService")
+    public MyService myNewService() {
+        return new MyServiceParamHello("new");
+    }
 
 }
